a80c35bc1007a5002dc9b5458d2e8e54ccba304a
Stoneman <sirckelbrad@gmail.com>
2026-06-22 21:43:05 +0700

    Decreased default settings to be more performant

--- src/main/java/net/stonenibbler/changed_survive_protocol/common/gamerule/CSPGameRules.java
+++ src/main/java/net/stonenibbler/changed_survive_protocol/common/gamerule/CSPGameRules.java
@@ -12,13 +12,13 @@ public final class CSPGameRules {
 
     public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_NEW_CHUNK_CHANCE = GameRules.register("csp:latexHeartNewChunkChance", GameRules.Category.UPDATES, GameRules.IntegerValue.create(400));
     public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_LOADED_CHUNK_CHANCE = GameRules.register("csp:latexHeartLoadedChunkChance", GameRules.Category.UPDATES, GameRules.IntegerValue.create(72000));
-    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_GROWTH_INTERVAL = GameRules.register("csp:latexHeartGrowthInterval", GameRules.Category.UPDATES, GameRules.IntegerValue.create(50));
-    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MAX_CLAIMS = GameRules.register("csp:latexHeartMaxClaims", GameRules.Category.UPDATES, GameRules.IntegerValue.create(75000));
-    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MAX_ACTIVE_PER_DIMENSION = GameRules.register("csp:latexHeartMaxActivePerDimension", GameRules.Category.UPDATES, GameRules.IntegerValue.create(128));
+    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_GROWTH_INTERVAL = GameRules.register("csp:latexHeartGrowthInterval", GameRules.Category.UPDATES, GameRules.IntegerValue.create(120));
+    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MAX_CLAIMS = GameRules.register("csp:latexHeartMaxClaims", GameRules.Category.UPDATES, GameRules.IntegerValue.create(12000));
+    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MAX_ACTIVE_PER_DIMENSION = GameRules.register("csp:latexHeartMaxActivePerDimension", GameRules.Category.UPDATES, GameRules.IntegerValue.create(32));
     public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MAX_NODES = GameRules.register("csp:latexHeartMaxNodes", GameRules.Category.UPDATES, GameRules.IntegerValue.create(6));
     public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MAX_NODE_DISTANCE = GameRules.register("csp:latexHeartMaxNodeDistance", GameRules.Category.UPDATES, GameRules.IntegerValue.create(32));
-    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_DECAY_INTERVAL = GameRules.register("csp:latexHeartDecayInterval", GameRules.Category.UPDATES, GameRules.IntegerValue.create(25));
-    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MOB_SPAWN_INTERVAL = GameRules.register("csp:latexHeartMobSpawnInterval", GameRules.Category.MOBS, GameRules.IntegerValue.create(600));
+    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_DECAY_INTERVAL = GameRules.register("csp:latexHeartDecayInterval", GameRules.Category.UPDATES, GameRules.IntegerValue.create(60));
+    public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MOB_SPAWN_INTERVAL = GameRules.register("csp:latexHeartMobSpawnInterval", GameRules.Category.MOBS, GameRules.IntegerValue.create(1200));
     public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MOB_SPAWN_MIN_CLAIMS = GameRules.register("csp:latexHeartMobSpawnMinClaims", GameRules.Category.MOBS, GameRules.IntegerValue.create(80));
     public static final GameRules.Key<GameRules.IntegerValue> LATEX_HEART_MOB_SPAWN_CAP = GameRules.register("csp:latexHeartMobSpawnCap", GameRules.Category.MOBS, GameRules.IntegerValue.create(8));
 
